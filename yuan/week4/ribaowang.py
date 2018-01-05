c08c894b90fcd77c2ae83592d14b18ae6cc05bd9
Alic-yuan <812933164@qq.com>
2018-01-05 14:33:47 +0800

    Update ribaowang.py

--- yuan/week4/ribaowang.py
+++ yuan/week4/ribaowang.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 
-MONGO_URL = 'mongodb://admin:yjl123@42.123.126.65:27027/'
+
 # MONGO_URL = 'localhost'
 MONGO_DB = 'xinwen'
 MONGO_TABLE = 'ribaowang_info'
@@ -19,8 +19,8 @@ def get_proxy():
         proxyPort = "9020"
 
         # 代理隧道验证信息
-        proxyUser = "HA0BA166652L8P6D"
-        proxyPass = "EDD43CF359807834"
+        proxyUser = ""
+        proxyPass = ""
 
         proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
           "host" : proxyHost,
