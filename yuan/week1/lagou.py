cfef1d73646286ed76cc8d1706eef01bbc5be60e
Alic-yuan <812933164@qq.com>
2018-01-05 14:26:53 +0800

    Update lagou.py

--- yuan/week1/lagou.py
+++ yuan/week1/lagou.py
@@ -8,7 +8,7 @@ import datetime
 from concurrent.futures import ThreadPoolExecutor
 
 
-# MONGO_URL = 'mongodb://admin:yjl123@42.123.126.65:27027/'
+
 MONGO_URL = 'localhost'
 MONGO_DB = 'lagou2'
 MONGO_TABLE = 'lagou_job2'
@@ -24,8 +24,8 @@ def get_proxy():
         proxyPort = "9020"
 
         # 代理隧道验证信息
-        proxyUser = "HA0BA166652L8P6D"
-        proxyPass = "EDD43CF359807834"
+        proxyUser = ""
+        proxyPass = ""
 
         proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
           "host" : proxyHost,
