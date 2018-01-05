14a22cc5d1ccbcc2a5c57b22d5628f83b2055431
Alic-yuan <812933164@qq.com>
2018-01-05 14:31:01 +0800

    Update beijing.py

--- yuan/week3/beijing.py
+++ yuan/week3/beijing.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from selenium import webdriver
 from multiprocessing import Pool
 
-MONGO_URL = 'mongodb://admin:yjl123@42.123.126.65:27027/'
+MONGO_URL = ''
 MONGO_DB = 'panjue'
 MONGO_TABLE = 'beijing_info'
 #
@@ -18,8 +18,8 @@ def get_proxy():
         proxyPort = "9020"
 
         # 代理隧道验证信息
-        proxyUser = "HA0BA166652L8P6D"
-        proxyPass = "EDD43CF359807834"
+        proxyUser = ""
+        proxyPass = ""
 
         proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
           "host" : proxyHost,
